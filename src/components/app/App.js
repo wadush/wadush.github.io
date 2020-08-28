5c1c03cfe0f2268a89d5fe1dedd9361187389a71
DCI-REC109 <you@example.com>
2020-08-28 16:44:23 +0200

    router, componnten

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "../header/Header";
 import Home from "../home/Home";
@@ -6,12 +7,18 @@ import Works from "../works/Works";
 import Contact from "../contact/Contact";
 
 function App() {
-  return <div className="App">
-    <Header />
-    <Home />
-    <Works />
-    <Contact />
-  </div>;
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/works" exact component={Works} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="Header">
       <h1>Wadush</h1>
       <nav className="Nav">
-        <h2>Nav</h2>
+        <ul className="ul-nav">
+          <li className="li-nav">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="li-nav">
+            <Link className="nav-link" to="/works">
+              Works
+            </Link>
+          </li>
+          <li className="li-nav">
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
