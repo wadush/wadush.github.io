debe5b093be4ec9889088227eb0b5831e414c72d
DCI-REC109 <you@example.com>
2020-08-28 15:48:43 +0200

    Updates

---
+++ precache-manifest.a1c1f98cc5f1368ba137ff8393e488a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4f84bd5c6cff50c1c0c7c41cdd0c2b",
+    "url": "/wadush/index.html"
+  },
+  {
+    "revision": "f098c4e026432b251e40",
+    "url": "/wadush/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "aa27fb19e6258d8e9731",
+    "url": "/wadush/static/js/2.237c139e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wadush/static/js/2.237c139e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f098c4e026432b251e40",
+    "url": "/wadush/static/js/main.4772c2e6.chunk.js"
+  },
+  {
+    "revision": "042939d947e6354ee7d8",
+    "url": "/wadush/static/js/runtime-main.d2fdbb2b.js"
+  }
+]);
