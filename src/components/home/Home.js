b317602cc386e20620e5313cd4134fef44cf134f
DCI-REC109 <you@example.com>
2020-10-02 13:20:59 +0200

    start Works

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <section className="Home main-section">
-      <h2 className="h2-home">Hello World</h2>
+      <h2 className="h2-home">Welcome</h2>
       <p className="p-home">
         My name is{" "}
         <Link className="Wadush" to="/about">
--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import './works.css';
+import React from "react";
+import "./works.css";
 
 function Works() {
   return (
-    <section className="Works main-section">
+    <section className="Works">
       <h2>Works</h2>
+      <ul className="cards__ul">
+        <li>
+          <h3>Cleancode</h3>
+        </li>
+        <li>
+          <h3>Cleancode</h3>
+        </li>
+        <li>
+          <h3>Cleancode</h3>
+        </li>
+        <li>
+          <h3>Cleancode</h3>
+        </li>
+      </ul>
     </section>
   );
 }
